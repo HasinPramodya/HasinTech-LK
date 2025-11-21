192f20748551e4e4c4a60f99b1c215ea6b865cd0
hasin.pramodya <pramodyahasin@gmail.com>
2025-11-21 08:14:48 +0530

    develop the HeroSection comnponent

---
+++ src/components/Home/HeroSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './HeroSection.css'
+
+
+export const HeroSection = ({title,subtitle,link,image}) => {
+  return (
+    <section className='hero_section'>
+        <div className="align-center">
+            <h2 className="hero_title">{title}</h2>
+            <p className="hero_subtitle">{subtitle}</p>
+            <a href={link} className='hero_link'>Buy Now</a>
+        </div>
+        <div className="align-center">
+            <img src={image} alt='' className='hero_image'/>
+        </div>
+    </section>
+  )
+}
