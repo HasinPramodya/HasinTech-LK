842c394a5d9df1aa379d9e6bc5bf964cece9bdc5
hasin.pramodya <pramodyahasin@gmail.com>
2025-11-22 19:36:39 +0530

    develop and styles productpage component

--- src/components/NavBar/NavLink/NavLink.jsx
+++ src/components/NavBar/NavLink/NavLink.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './NavLink.css';
 
 
-export const NavLink = ({title,link, emoji}) => {
+export const NavLink = ({title,link, emoji,sidebar}) => {
   return (
-     <a href={link} className='align-center'>{title}<img src={emoji} alt='' className='link_emoji'/></a>
+     <a href={link} className={sidebar ? 'align-center sidebar_link' : 'align-center'}>{title}<img src={emoji} alt='' className='link_emoji'/></a>
   )
 }
