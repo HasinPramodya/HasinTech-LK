8db8e71414e52630b60d8e2c8d4f95c6d9bc27d0
hasin.pramodya <pramodyahasin@gmail.com>
2025-11-22 23:53:37 +0530

    develop and styles myorderpage component

---
+++ src/components/MyOrder/MyOrderPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './MyOrderPage.css'
+import { Table } from '../Common/Table'
+
+export const MyOrderPage = () => {
+  return (
+    <section className="align-center myorder_page">
+        <Table headings={["Order", "Products", "Total", "Status"]}>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>iPhone, Power Bank</td>
+                    <td>$1299</td>
+                    <td>Shipped</td>
+                </tr>
+            </tbody>
+        </Table>
+    </section>
+  )
+}
