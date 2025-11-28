d7f45066cec30e7e363942e3330c3068ce181770
hasin.pramodya <pramodyahasin@gmail.com>
2025-11-28 13:30:01 +0530

    fetch categories from API and render in ProductsSidebar component

--- src/components/Products/ProductsSidebar.jsx
+++ src/components/Products/ProductsSidebar.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './productsSidebar.css'
 import { NavLinks } from '../NavBar/NavLinks/NavLinks'
-import rocket from '../../assets/rocket.png'
+import apiClient from '../../utils/api-client'
 
 export const ProductsSidebar = () => {
+  const [categories, setCategories] = useState([]);
+  const [erros,setErrors] = useState([]);
+
+  useEffect(()=>{
+      apiClient.get("/category").then(
+        (res)=>{
+            setCategories(res.data);
+            console.log(res.data);
+        }
+      ).catch(
+        (err)=>{
+            setErrors(err.meassge);
+        }
+      )
+  },[])
+
   return (
     <aside className="products_sidebar">
         <h2>Category</h2>
         <div className="category_links">
-            <NavLinks title="Electronics" link="products?category=electronics" emoji={rocket} sidebar={true} />
+          {
+            categories.map((category)=>{
+              return <NavLinks key={category.name} title={category.name} link={`products?category=${category.name}`} emoji={`http://localhost:5000/category/${category.image}`}  sidebar={true} />
+            })
+          }
+           
         </div>
     </aside>
   )
