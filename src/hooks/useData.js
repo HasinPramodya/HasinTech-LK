112cbc6014c5c723f9a94db8e2166b3e702b3e7f
hasin.pramodya <pramodyahasin@gmail.com>
2025-11-29 19:22:49 +0530

    add isLoading propety to the useData hook

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -5,18 +5,21 @@ import  apiClient  from '../utils/api-client'
 export const useData = (url) => {
   const [data, setData] = useState(null);
     const [erros,setErrors] = useState([]);
+    const [isLoading, setIsloading] = useState(false);
   
     useEffect(()=>{
+        setIsloading(true);
         apiClient.get(url).then(
           (res)=>{
               setData(res.data);
-              console.log(res.data);
+              setIsloading(false);
           }
         ).catch(
           (err)=>{
               setErrors(err.meassge);
+              setIsloading(false);
           }
         )
     },[]);
-    return {data,erros};
+    return {data,erros, isLoading};
 }
