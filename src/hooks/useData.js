33b6ae13a2b231276e8ca496cd472e95ffb96839
hasin.pramodya <pramodyahasin@gmail.com>
2025-11-29 20:04:03 +0530

    refactor: update useData hook to accept configObject and dependencies

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import  apiClient  from '../utils/api-client'
 
-export const useData = (url) => {
+export const useData = (endpoint, configObject, dependencies) => {
   const [data, setData] = useState(null);
     const [erros,setErrors] = useState([]);
     const [isLoading, setIsloading] = useState(false);
   
     useEffect(()=>{
         setIsloading(true);
-        apiClient.get(url).then(
+        apiClient.get(endpoint,configObject).then(
           (res)=>{
               setData(res.data);
               setIsloading(false);
@@ -20,6 +20,6 @@ export const useData = (url) => {
               setIsloading(false);
           }
         )
-    },[]);
+    },dependencies ? dependencies : []);
     return {data,erros, isLoading};
 }
