ec56a57e8d95f0bd8cca6e08d3e92a8ef44ab19b
hasin.pramodya <pramodyahasin@gmail.com>
2025-12-03 14:32:00 +0530

    add user object state for app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import "./App.css";
 import { Routing } from "./components/Routing/Routing";
 import { NavBar } from "./components/NavBar/NavBar";
+import { useEffect, useState } from "react";
+import { jwtDecode } from "jwt-decode";
 
 
 
 function App() {
+  const [user, setUser] = useState(null);
+
+ useEffect(() => {
+  try {
+     const jwt =localStorage.getItem("token");
+    const jwtUser = jwtDecode(jwt);
+    if(Date.now() >= jwtUser.exp * 1000){
+      localStorage.removeItem("token");
+      location.reload();
+    }else{
+      setUser(jwtUser);
+    }  
+  }catch (error) {}
+      
+ },[user])
   return (
     <>
       <div className="app">
-        <NavBar />
+        <NavBar user={user}/>
         <main>
           <Routing />
         </main>
